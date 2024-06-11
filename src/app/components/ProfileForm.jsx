2e9b76d08fdfcbbee2d17b89d839c6da3d98d16f
DAVINA SINGH <davina_singh@outlook.com>
2024-06-11 10:45:54 +0100

    error

--- src/app/components/ProfileForm.jsx
+++ src/app/components/ProfileForm.jsx
@@ -3,22 +3,23 @@ import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 export default function ProfileForm() {
-  // this get user id from clerk
   const { userId } = auth();
   async function handleUpdateProfile(formData) {
     "use server";
     const username = formData.get("username");
+
     await db.query(
       `UPDATE profiles SET username = '${username}' WHERE clerk_id = '${userId}'`
     );
     revalidatePath("/");
   }
+
   return (
     <div>
-      <h1>Update Profile</h1>
-      <p>Welcome, please choose a username</p>
+      <h2>Update Profile</h2>
+      <p>Welcome to TimTalk, please choose your username!</p>
       <form action={handleUpdateProfile}>
-        <input name="username" placeholder="username" />
+        <input name="username" placeholder="Username" />
         <button>Submit</button>
       </form>
     </div>
